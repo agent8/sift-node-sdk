7f4c6fa9206f901d5361234271c8788667725011
Bili Xu <xubili93@gmail.com>
2016-04-04 17:36:39 -0700

    Add test cases

--- test/siftapi-test.js
+++ test/siftapi-test.js
@@ -30,18 +30,100 @@ describe('SiftAPI - generateSignature tests', () => {
 
   beforeEach(() => {
     // Initialize with dummy API and Secret to test signature generation
-    siftapi = new SiftAPI('abc', '123');
+    siftapi = new SiftAPI(API_KEY, API_SECRET);
   });
 
   it('should generate the correct signature', () => {
-    const path = '/users/test/sifts';
-    const time = 1459546790;
-    const params = siftapi._generateParams(time);
-
-    const inpt = siftapi._generateSignature('get', path, params)
-    const output = '7ebea0370e02ac6483d95e309d25acd6c327d7f0';
+    const path = '/users';
+    const time = 1459805820;
+    const params = {
+      ...siftapi._generateParams(time)
+    };
+    const data = {
+      username: 'test',
+      locale: 'en_US'
+    };
+    const inpt = siftapi._generateSignature('POST', path, params, data);
+    const output = '591c523f5b112c5e6be4006bafd812d0ec803bff';
     expect(inpt).to.be.equal(output);
   });
 
+});
+
+describe('SiftAPI - _request tests', () => {
+  let siftapi;
+
+  beforeEach(() => {
+    siftapi = new SiftAPI(API_KEY, API_SECRET);
+  });
+
+  it('should make a request with the correct params', (done) => {
+    const path = '/users/test/sifts';
+    siftapi._request('GET', path)
+      .then(body => {
+        expect(body.code).to.be.equal(200);
+        done();
+      })
+      .catch(err => {
+        expect(err).to.be.throw;
+        done();
+      });
+  });
+});
+
+describe('SiftAPI - API endpoint tests', () => {
+  let siftapi;
+
+  beforeEach(() => {
+    siftapi = new SiftAPI(API_KEY, API_SECRET);
+  });
+
+  it('should create a new user', done => {
+    siftapi.addUser('test', 'en_US')
+      .then(body => {
+        expect(body.message).to.be.equal('success');
+        expect(body.code).to.be.equal(200);
+        expect(body.result.username).to.be.equal('test');
+        done();
+      }, err => {
+        console.log('Error!');
+        console.log(err);
+      });
+  });
+
+  it('should get list of email connections', done => {
+    siftapi.getEmailConnections('test')
+      .then(body => {
+        expect(body.code).to.be.equal(200);
+        done();
+      }, err => {
+        console.log('Error!');
+        console.log(err);
+      });
+  });
+
+  it('should get list of sifts', done => {
+    siftapi.getSifts('test')
+      .then(body => {
+        expect(body.code).to.be.equal(200);
+        done();
+      }, err => {
+        console.log('Error!');
+        console.log(err);
+      });
+  });
+
+  it('should delete a new user', done => {
+      siftapi.deleteUser('test')
+        .then(body => {
+          expect(body.message).to.be.equal('success');
+          expect(body.code).to.be.equal(200);
+          expect(body.result).to.be.equal('deleted');
+          done();
+        }, err => {
+          console.log('Error!');
+          console.log(err);
+        });
+    });
 });
 

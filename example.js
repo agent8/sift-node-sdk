f7d3c7025a08660d225020dcfee8437a47d64cd7
Bili Xu <xubili93@gmail.com>
2016-04-06 14:00:50 -0700

    Update example code

--- example.js
+++ example.js
@@ -1,3 +1,46 @@
 'use strict';
 
-/*global require*/
+var fs = require('fs');
+var SiftAPI = require('./dist/siftapi').default;
+
+// Fill in with your own API key and secret
+var sift = new SiftAPI('', '');
+
+sift.addUser('test', 'en_US')
+  .then(body => {
+    console.log(body);
+  });
+
+/**
+ * Remember to include your own test eml file in the same directory as
+ * example.js
+ */
+fs.readFile('test.eml', 'utf8', (err, eml) => {
+  if(err) {
+    return console.log(err);
+  }
+
+  sift.discovery('test', eml.toString())
+    .then(body => {
+      console.log(body);
+    })
+    .catch(err => {
+      return console.log('err');
+    });
+});
+
+sift.getSifts('test')
+  .then(body => {
+    console.log(body);
+  });
+
+sift.postFeedback('test', 'en_US', 'America/Los_Angeles')
+  .then(body => {
+    console.log(body);
+  });
+
+sift.deleteUser('test')
+  .then(body => {
+    console.log(body);
+  });
+

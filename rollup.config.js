6b93b4803ae1d8b26f61620fdf830e7bf2d2d5f7
Bili Xu <bili@edison.tech>
2019-02-22 13:26:30 -0800

    Remove unused Rollup plugins and test dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
-import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
 
 export default {
   input: 'src/siftapi.js',
@@ -14,11 +12,7 @@ export default {
   plugins: [
     json(),
     commonjs(),
-    globals(),
-    resolve({
-      preferBuiltins: true
-    }),
-    builtins(),
+    resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
